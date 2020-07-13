4dcf1a30e66f6188adb2dac09bc7f2eaa4a35f22
Shivam Kumar <shivam.saha06@gmail.com>
2020-07-13 22:17:13 +0530

    Command Line Location Ready

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const address = process.argv[2]
 
-
-geocode('New York',(error,data) => {
- console.log('Error', error)
- console.log('Data', data)
-})
-
-
-
-forecast(-73.9808, 40.7648, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+if(!address){
+  console.log('Please provide an address')
+} else{
+  geocode(address,(error,data) => {
+    if(error){
+      return console.log(error)
+    }
+   
+   forecast(data.latitude, data.longitude, (error, forecastData) => {
+     if(error){
+       return console.log(error)
+     }
+   console.log(data.location)
+   console.log(forecastData)
   })
+  })
+}
