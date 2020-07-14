1d91343ad11ac1b516582593b590b753583cd2d4
Shivam Kumar <shivam.saha06@gmail.com>
2020-07-14 22:18:33 +0530

    Final Commit

--- utils/forecast.js
+++ utils/forecast.js
@@ -4,14 +4,14 @@ const forecast = (latitude , longitude , callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=143a7edb23ee19267fd9f7d97aee04b5&query='+latitude+','+longitude
 
 
-    request({url: url , json: true}, (error,response) => {
+    request({url , json: true}, (error,{body}) => {
 
         if(error){
             callback('Unable to connect to weather services', undefined)
-        }else if(response.body.error){
+        }else if(body.error){
             callback('Unable to find location',undefined)
         }else { 
-            callback(undefined, response.body.current.weather_descriptions[0] + ". The is currently " + response.body.current.temperature + " degrees out. " +"It feels like humidity is "+ response.body.current.humidity+ "% out there.")
+            callback(undefined, body.current.weather_descriptions[0] + ". The is currently " + body.current.temperature + " degrees out. " +"It feels like humidity is "+ body.current.humidity+ "% out there.")
 
         }
     })
